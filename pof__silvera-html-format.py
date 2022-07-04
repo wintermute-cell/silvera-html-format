a9bdff4602b8ce5f5e9decdd739d625b8a6f9aed
wintermute-cell <kjulius@gmx.de>
2022-07-04 11:52:53 +0200

    changed default, little cleanup

--- pof__silvera-html-format.py
+++ pof__silvera-html-format.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
+# CONFIGURATION
+INDENT_WITH = 2
+
+# CONFIGURATION END
+
 import re
 import sys
 from bs4 import BeautifulSoup as bs
 
 # this is used to hack custom indent width on prettify()
 r = re.compile(r'^(\s*)', re.MULTILINE)
-def prettify_cust_space(s, encoding=None, formatter="minimal", indent_width=2):
-    return r.sub(r'\1'*indent_width, s.prettify(encoding, formatter))
+def prettify_cust_space(s, encoding=None, formatter="minimal", width=2):
+    return r.sub(r'\1'*width, s.prettify(encoding, formatter))
 
 file_path = sys.argv[1]
 if file_path is not None and file_path != "":
@@ -15,7 +20,7 @@ if file_path is not None and file_path != "":
 
     with open(file_path, "r") as f:
         s = bs(f.read(), features='lxml')
-        formatted = prettify_cust_space(s, indent_width=4)
+        formatted = prettify_cust_space(s, width=INDENT_WITH)
 
     with open(file_path, "w") as f:
         f.write(formatted)
