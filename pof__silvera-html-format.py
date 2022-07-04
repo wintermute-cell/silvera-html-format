6bb42f5206256b453968f12e42a9d9c57bb005f2
wintermute-cell <kjulius@gmx.de>
2022-07-04 11:15:30 +0200

    implement custom indent size

--- pof__silvera-html-format.py
+++ pof__silvera-html-format.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 from bs4 import BeautifulSoup as bs
+
+# this is used to hack custom indent width on prettify()
+r = re.compile(r'^(\s*)', re.MULTILINE)
+def prettify_cust_space(s, encoding=None, formatter="minimal", indent_width=2):
+    return r.sub(r'\1'*indent_width, s.prettify(encoding, formatter))
 
 file_path = sys.argv[1]
 if file_path is not None and file_path != "":
@@ -9,7 +15,7 @@ if file_path is not None and file_path != "":
 
     with open(file_path, "r") as f:
         s = bs(f.read(), features='lxml')
-        formatted = s.prettify()
+        formatted = s.prettify_cust_space(indent_width=4)
 
     with open(file_path, "w") as f:
         f.write(formatted)
